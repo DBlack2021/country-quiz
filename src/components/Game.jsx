1a89c7ac6e9ef38c8645662b5c03a70f2045fb2d
Dylan Black <dylan.black2021@gmail.com>
2021-06-20 08:55:21 -0400

    question filling

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { TestQuestion } from '../TestData/TestData'
 
 export default function Game({ numQuestions }) {
+
+  const [currentQ, setCurrentQ] = useState(0)
+  const [questions, setQuestions] = useState([]);
+  useEffect(() => {
+    const qArr = new Array(5).fill(TestQuestion);
+    setQuestions(Array.from(qArr));
+    //generate questions?
+    return () => {
+      //unmounting
+    }
+
+  }, [])
+
   return (
     <div>
-      {numQuestions}
+      {questions.map(question => (
+        <p key={question.question}>{question.question}</p>
+      ))}
     </div>
   )
 }
